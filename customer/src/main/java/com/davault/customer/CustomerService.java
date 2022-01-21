33aefcad62279ce7bdf7051b6fa53eb699a34ccc
Joe Davault <joe.davault@gmail.com>
2022-01-21 13:17:30 -0700

    added the Eureka Server

--- customer/src/main/java/com/davault/customer/CustomerService.java
+++ customer/src/main/java/com/davault/customer/CustomerService.java
@@ -5,6 +5,9 @@ import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
 @Service
 @AllArgsConstructor
 
